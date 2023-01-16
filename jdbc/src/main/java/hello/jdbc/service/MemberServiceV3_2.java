2d8fd41a9ac2cc61b39af238367775e74346ec28
zieunx <wldms_1217@naver.com>
2023-01-16 22:26:18 +0900

    feat: 계좌이체실습 - TransactionTemplate 으로 반복되는 패턴 실행해주도록 변경. 여전히 service 에 transaction 에 대한 관심사는 남아있음.

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -0,0 +1,52 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 템플릿
+ */
+@Slf4j
+public class MemberServiceV3_2 {
+
+    private final TransactionTemplate txTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        // TransactionTemplate 생성할 때 transactionManager 가 필요하기 때문에 주입은 transactionManager 로 받는다.
+        // 이 패턴으로 많이 사용함 (관례적인 부분, 인터페이스로 관계를 맺기 위해)
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        txTemplate.executeWithoutResult(transactionStatus -> {
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                // 트랜잭션 - 롤백
+                throw new IllegalStateException(e);
+            }
+        });
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
