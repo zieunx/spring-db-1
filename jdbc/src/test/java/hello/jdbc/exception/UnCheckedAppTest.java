7caeb8c5a5d51c776bc9d6277ed02c3b2fd72820
zieunx <wldms_1217@naver.com>
2023-01-17 10:29:50 +0900

    feat: 예외 - checked -> unchecked 예외변환. 어짜피 처리할 수 없는 연쇄적 체크 에러 의존성 제거

---
+++ jdbc/src/test/java/hello/jdbc/exception/UnCheckedAppTest.java
@@ -0,0 +1,67 @@
+package hello.jdbc.exception;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UnCheckedAppTest {
+
+    @Test
+    void unchecked() {
+        Controller controller = new Controller();
+        assertThatThrownBy(controller::request)
+                .isInstanceOf(RunTimeSqlException.class);
+    }
+
+    static class Controller {
+        public void request() {
+            Service service = new Service();
+            service.logic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() {
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+    static class NetworkClient {
+        public void call() {
+            throw new RunTimeConnectException("연결 실패");
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            try {
+                runSQL();
+            } catch (SQLException e) {
+                // Runtime 예외로 바꿔서 던짐
+                throw new RunTimeSqlException(e);
+            }
+        }
+
+        public void runSQL() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+    static class RunTimeConnectException extends RuntimeException {
+        public RunTimeConnectException(String message) {
+            super(message);
+        }
+    }
+
+    static class RunTimeSqlException extends RuntimeException {
+        public RunTimeSqlException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
